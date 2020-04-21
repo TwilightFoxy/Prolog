63ce393f0fae3ee00119760efcf1f43c4dba6922
Levon <5novel5@mail.ru>
2020-04-21 10:18:39 +0300

    F
    Базовые файлы фшарпа

---
+++ Fsharp1/Fsharp1/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+let sum x y = x+y
+let pr x y = x*y
+let p f x y = f x y
+
+let rec fact f n =
+        match n with 
+        | 1 -> 1
+        | _ ->  let v = fact f (n-1) in
+                p f n v 
+
+let p9_3 =
+    let b = System.Convert.ToInt32(System.Console.ReadLine())
+    (fact sum b,fact pr b)
+
+
+[<EntryPoint>]
+let main argv = 
+    
+    
+    let Ans = p9_3
+    System.Console.Write(Ans)
+    let z = System.Console.ReadKey()
+    
+    0 // return an integer exit code
---
+++ PZ1. max/PZ1. max/Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+//let max2 x y =
+//    if x>y then x
+//    else y
+let max3 x y z =
+    if (x>y)&&(x>z) then x
+    elif y>z then y
+    else z
+
+//let p1_1 = 
+//    let b = System.Convert.ToInt32(System.Console.ReadLine())
+//    let c = System.Convert.ToInt32(System.Console.ReadLine())
+//    max2 b c
+
+let p1_2 =
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    let b = System.Convert.ToInt32(System.Console.ReadLine())
+    let c = System.Convert.ToInt32(System.Console.ReadLine())
+    max3 a b c
+
+
+[<EntryPoint>]
+let main argv =
+//    let Ans = p1_1
+//    System.Console.Write(Ans)
+//    let z = System.Console.ReadKey()
+    let Ans1 = p1_2
+    System.Console.Write(Ans1)
+    let z = System.Console.ReadKey()
+    0 // return an integer exit code
