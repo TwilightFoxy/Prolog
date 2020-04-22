e494b20e919d6429745f5d0f31b71cede1748f77
Levon <5novel5@mail.ru>
2020-04-22 16:07:33 +0300

    Ind1
    Исправил выход в отрицательные и нулевые числа

--- ind1.pl
+++ ind1.pl
@@ -1,6 +1,12 @@
 perebor(0):-!.
 perebor(X):- write(X),X1 is X-1, perebor(X1).
-write3(X,Y,Z):- write(X), write(Y), write(Z).
+write3(X,Y,Z):- write(X),write(" "),write(Y),write(" "),write(Z),write("\n").
 prov(X,Y,Z):- X<Y+Z, Y<X+Z, Z<X+Y.
-start(X):-prov(X,2,1),write3(X,2,1),X1 is X-1, start(X1).
+start(0):-!.
+start(X):- prov(X,2,1),write3(X,2,1),X1 is X-1, start(X1).
 start(X):- not(prov(X,2,1)),X1 is X-1,start(X1).
+
+one(0,_):-!.
+one(X,P):-Y is P-3,two(X,Y,P).
+two(X,0,P):-X1 is X-1,one(X1,P).
+two(X,Y,P):-Z is P-X-Y, Y1 is Y-1,write3(X,Y,Z), two(X,Y1,P).
