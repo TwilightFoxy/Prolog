de4679a814d5b4a3e56bf5d6778b8ec620baa487
Levon <5novel5@mail.ru>
2020-04-22 16:07:33 +0300

    Ind1
    Работаю над перебором трех элементов, по типу перебора x и y и z=p-x-y

--- Fsharp1/Fsharp1/Program.fs
+++ Fsharp1/Fsharp1/Program.fs
@@ -1,26 +1,26 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-let sum x y = x+y
-let pr x y = x*y
-let p f x y = f x y
+﻿let non_del X Y = 
+    if X % Y =0   then false
+                  else true
 
-let rec fact f n =
-        match n with 
-        | 1 -> 1
-        | _ ->  let v = fact f (n-1) in
-                p f n v 
+let rec pros X Y =
+        if X=Y then true
+        else (non_del X Y) && (pros X (Y+1))
 
-let p9_3 =
-    let b = System.Convert.ToInt32(System.Console.ReadLine())
-    (fact sum b,fact pr b)
 
+let pr n = 
+    match n with
+    | 2 -> true
+    | _ -> pros n 2 
+
+let p9_5 = 
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    pr a
 
 [<EntryPoint>]
 let main argv = 
     
-    
-    let Ans = p9_3
+    let Ans = p9_5
     System.Console.Write(Ans)
-    let z = System.Console.ReadKey()
+    let z=System.Console.ReadKey()
     
     0 // return an integer exit code
