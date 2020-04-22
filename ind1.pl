6335ccf92a1513c8ebab3a759b27ab444d52ed7a
Levon <5novel5@mail.ru>
2020-04-22 18:35:35 +0300

    Ind1
    Индивидуалка готова, добавил счетчик для подсчета количества возможных треугольников.

--- ind1.pl
+++ ind1.pl
@@ -1,13 +1,12 @@
-perebor(0):-!.
-perebor(X):- write(X),X1 is X-1, perebor(X1).
 write3(X,Y,Z):- write(X),write(" "),write(Y),write(" "),write(Z),write("\n").
 prov(X,Y,P):- Z is P-X-Y, X<Y+Z, Y<X+Z, Z<X+Y.
 %start(0):-!.
 %start(X):- prov(X,2,1),write3(X,2,1),X1 is X-1, start(X1).
 %start(X):- not(prov(X,2,1)),X1 is X-1,start(X1).
 
-one(0,_):-!.
-one(X,P):- Y is P-X,two(X,Y,P).
-two(X,Y,P):- Y=0, X1 is X-1,one(X1,P).
-two(X,Y,P):- prov(X,Y,P),Z1 is P-X-Y, write3(X,Y,Z1),Y1 is Y-1,two(X,Y1,P).
-two(X,Y,P):- Y1 is Y-1, two(X,Y1,P).
+ind1():-read(P),X is P-1,one(X,P,0).
+one(X,_,K):- X=0, write("Всего "),write(K),write(" треугольников."),!.
+one(X,P,K):- Y is P-X,two(X,Y,P,K).
+two(X,Y,P,K):- Y=0, X1 is X-1,one(X1,P,K).
+two(X,Y,P,K):- prov(X,Y,P),Z1 is P-X-Y, write3(X,Y,Z1),Y1 is Y-1,K1 is K+1,two(X,Y1,P,K1).
+two(X,Y,P,K):- Y1 is Y-1, two(X,Y1,P,K).
