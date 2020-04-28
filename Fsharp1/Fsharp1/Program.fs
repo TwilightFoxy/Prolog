634e40226e66b91562ff18400a56913128c21487
Levon <5novel5@mail.ru>
2020-04-28 09:52:20 +0300

    ind9
    Эм... работает)

--- Fsharp1/Fsharp1/Program.fs
+++ Fsharp1/Fsharp1/Program.fs
@@ -32,25 +32,42 @@ let rec loop n =
 
 let rec fact n =
     match n with
+    | 0->1
     | 1->1
+    | 2->2
     | _-> n * fact (n-1)
 
 let rec ind1 n sum =
+    //printfn "%d,%d" n sum
 
-    printfn "%d,%d" n sum
+    //if (n%10=0)&&(n>0) then ind1 (n/10) sum
+    //else
     match n with
-    | 1-> sum+1
+    | 0 -> sum+0
+    | 1 -> sum+1
     | _ -> ind1 (n/10) (sum + fact (n%10))
+
+let rec ch a =
+    let V = ind1 a 0
+    //printfn "%d" V
+    if (V = a) then printfn "%d" a 
+    match a with
+    | 1 -> 1
+    | _ -> ch (a-1) 
+
+
 
 let p9_5 = 
     let a = System.Convert.ToInt32(System.Console.ReadLine())
-    ind1 a 0
+    ch a 
+    //ind1 a 0
+    //loop a
 
 [<EntryPoint>]
 let main argv = 
     
     let Ans = p9_5
-    System.Console.Write(Ans)
+    //System.Console.Write(Ans)
 
     let z=System.Console.ReadKey()
     
